dee567a27548fff5ed0185684b4dde6593e180e3
Luke W Johnston <lwjohnst@gmail.com>
2016-10-06 12:58:00 -0400

    Changed to Rstudio website builder

--- build.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/Rscript
-
-rmarkdown::render_site()
-
-# rmarkdown::render('index.Rmd', output_format = 'pdf_document',
-#                    output_options = list(latex_engine = 'xelatex',
-#                                          template = '_includes/style/style.tex'))
