03d30343d2046e33ad27382264b09a5216494553
Luke W Johnston <lwjohnst@gmail.com>
2017-01-16 17:58:57 -0500

    Added first published and title for CRAN pkg

--- R/rpackages.R
+++ R/rpackages.R
@@ -3,7 +3,8 @@ output <- yaml::yaml.load_file('_includes/rpackages.yaml')
 
 cat('\n\n### CRAN\n\n')
 for (i in output$rpackages$cran) {
-    cat(i$pkg, '\n: ', i$details, '\n\n')
+    cat(i$pkg, '\n: ', paste0('_', i$title, '_: '), i$details, ' *First published*: ',
+        convert_date(i$first_published), '\n\n')
 }
 
 cat('\n\n### GitHub\n\n')
