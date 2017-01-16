756d92d878046c9734751a520fffeb6edad20a69
Luke W Johnston <lwjohnst@gmail.com>
2017-01-16 17:58:11 -0500

    Resolves #22

--- R/education.R
+++ R/education.R
@@ -7,12 +7,16 @@ ordered_output <- order_by_year(output)
 
 cat('\n### ', N[1], '\n\n')
 for (i in ordered_output[[n[1]]]) {
+    co <- ''
+    if (!is.null(i$co_supervisor))
+        co <- paste0('. Co-supervisor: ', i$co_supervisor)
+
     thesis <- ''
     if (!is.null(i$thesis) & !is.null(i$link)) {
         thesis <- paste0('[', i$thesis,
-                         '](', i$link, ') Supervisor: ', i$supervisor)
+                         '](', i$link, ') Supervisor: ', i$supervisor, co)
     } else if (!is.null(i$thesis) & is.null(i$link)) {
-        thesis <- paste0(i$thesis,' Supervisor: ', i$supervisor)
+        thesis <- paste0(i$thesis,' Supervisor: ', i$supervisor, co)
     }
     cat(i$end, '\n: *',
         i$degree, '*, ', i$university, '\n\n\t',
