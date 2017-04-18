19b6f62dde81e030d3d2b78b8dd56865636ff717
Luke W Johnston <lwjohnst@gmail.com>
2017-04-18 16:50:07 -0400

    Resolves #24, resolves #19

--- R/education.R
+++ R/education.R
@@ -3,10 +3,11 @@ output <- yaml.load_file('_includes/education.yaml')
 n <- names(output)
 N <- gsub('^([a-z])', '\\U\\1', n, perl = TRUE)
 
-ordered_output <- order_by_year(output)
+ordered_degrees <- order_by_year(output$degrees)
+ordered_certificates <- order_by_year(output$certificates)
 
 cat('\n### ', N[1], '\n\n')
-for (i in ordered_output[[n[1]]]) {
+for (i in ordered_degrees) {
     co <- ''
     if (!is.null(i$co_supervisor))
         co <- paste0('. Co-supervisor: ', i$co_supervisor)
@@ -18,13 +19,16 @@ for (i in ordered_output[[n[1]]]) {
     } else if (!is.null(i$thesis) & is.null(i$link)) {
         thesis <- paste0(i$thesis,' Supervisor: ', i$supervisor, co)
     }
+
+    if (is.null(i$end))
+        i$end <- "Present"
     cat(i$end, '\n: *',
         i$degree, '*, ', i$university, '\n\n\t',
         thesis, '\n\n', sep = '')
 }
 
 cat('\n### ', N[2], '\n\n')
-for (i in ordered_output[[n[2]]]) {
+for (i in ordered_certificates) {
     website <- ''
     if (!is.null(i$website))
         website <- paste0('(',i$website,')')
