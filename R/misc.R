69b197c600873ad943b2c3e65436dfbe283116f1
Luke W Johnston <lwjohnst@gmail.com>
2017-08-27 19:46:52 -0400

    fixed item list formatting problem

--- R/misc.R
+++ R/misc.R
@@ -2,9 +2,9 @@ source('R/functions.R')
 output <- yaml::yaml.load_file('_includes/misc.yaml')
 
 cat('\n\n### Programming languages\n\n')
-cat('Competent\n: - ', paste0(output$skills$programming$competent, collapse = '\n\t- '), '\n\n')
-cat('Familiar\n: - ', paste0(output$skills$programming$familiar, collapse = '\n\t- '), '\n\n')
+cat('**Competent**\n\n- ', paste0(output$skills$programming$competent, collapse = '\n- '), '\n\n')
+cat('**Familiar**\n\n- ', paste0(output$skills$programming$familiar, collapse = '\n- '), '\n\n')
 
 cat('\n\n### Statistical techniques\n\n')
-cat('Competent\n: - ', paste0(output$skills$statistics$competent, collapse = '\n\t- '), '\n\n')
-cat('Familiar\n: - ', paste0(output$skills$statistics$familiar, collapse = '\n\t- '), '\n\n')
+cat('**Competent**\n\n- ', paste0(output$skills$statistics$competent, collapse = '\n- '), '\n\n')
+cat('**Familiar**\n\n- ', paste0(output$skills$statistics$familiar, collapse = '\n- '), '\n\n')
