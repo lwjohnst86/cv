532af88b22644e71f57a3c72ead5a2e8f01c0446
Luke W Johnston <lwjohnst@gmail.com>
2017-09-18 16:50:16 -0400

    generate pdf of cv

--- to-pdf.sh
+++ to-pdf.sh
@@ -1,1 +1,2 @@
 pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine index.html -o index.pdf
+pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine recent.html -o recent.pdf
