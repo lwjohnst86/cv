0b943f2b2423323c174724e3243e9cb32c792c4e
Luke W Johnston <lwjohnst@gmail.com>
2017-11-15 15:49:17 -0500

    made a custom build command with render + pandoc

--- _build.sh
+++ _build.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+
+Rscript -e "rmarkdown::render_site('.', encoding = 'UTF-8')"
 pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine index.html -o index.pdf
+mv index.pdf cv-full-lukewjohnston.pdf
 pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine recent.html -o recent.pdf
+mv recent.pdf cv-recent-lukewjohnston.pdf
