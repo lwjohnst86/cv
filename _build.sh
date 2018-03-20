5f92582938dc89f9592aba6b0c7668d9fec2b0c7
Luke W Johnston <lwjohnst@gmail.com>
2018-03-20 22:15:50 +0100

    use md instead of html for pdf convert

--- _build.sh
+++ _build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 Rscript -e "rmarkdown::render_site('.', encoding = 'UTF-8')"
-pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine index.html -o index.pdf
+pandoc --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine index.md -o index.pdf
 mv index.pdf cv-full-lukewjohnston.pdf
-pandoc -V title:"" --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine recent.html -o recent.pdf
+pandoc --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt -V fontfamily=libertine recent.md -o recent.pdf
 mv recent.pdf cv-recent-lukewjohnston.pdf
+
