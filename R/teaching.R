c9483bbea5b1e09fd4ccd3ffcf0d688a21720cbe
Luke Johnston <lwjohnst@gmail.com>
2018-11-25 15:35:56 +0100

    use glue

--- R/teaching.R
+++ R/teaching.R
@@ -6,7 +6,7 @@ ordered_output <- order_by_year(output)
 for (i in ordered_output) {
     if (restricted_entry(i)) next
     if (is.null(i$end)) {
-        timeline <- paste0(convert_date(i$start), ' -- present')
+        timeline <- glue::glue("{convert_date(i$start)} --- present")
     } else {
         timeline <- convert_date(i$start, i$end)
     }
