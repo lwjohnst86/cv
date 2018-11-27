4d5886cac49e3b73fb31bf831e071bde29405e06
Luke Johnston <lwjohnst@gmail.com>
2018-11-27 14:31:48 +0100

    add timeline to degrees etc

--- R/education.R
+++ R/education.R
@@ -9,31 +9,45 @@ ordered_courses <- order_by_year(output$courses)
 
 cat('\n### ', N[1], '\n\n')
 for (i in ordered_degrees) {
-    co <- ''
+    if (is.null(i$end)) {
+        timeline <- paste0(convert_date(i$start), ' -- present')
+    } else if (is.null(i$start)) {
+        timeline <- convert_date(i$end)
+    } else {
+        timeline <- convert_date(i$start, i$end)
+    }
+
+    co <- ""
     if (!is.null(i$co_supervisor))
-        co <- paste0('. Co-supervisor: ', i$co_supervisor)
+        co <- glue("Co-supervisor: {i$co_supervisor}")
+
+    supervisor <- ""
+    if (!is.null(i$supervisor))
+        supervisor <- glue("Supervisor: {i$supervisor}")
 
     thesis <- ''
     if (!is.null(i$thesis) & !is.null(i$link)) {
-        thesis <- paste0('[', i$thesis,
-                         '](', i$link, ') Supervisor: ', i$supervisor, co)
+        thesis <- glue('[{i$thesis}]({i$link}) {supervisor}. {co}')
     } else if (!is.null(i$thesis) & is.null(i$link)) {
-        thesis <- paste0(i$thesis,' Supervisor: ', i$supervisor, co)
+        thesis <- glue('{i$thesis} {supervisor}. {co}')
     }
 
-    if (is.null(i$end))
-        i$end <- "Present"
-    cat(i$end, '\n: *',
+    cat(timeline, '\n: *',
         i$degree, '*, ', i$university, '\n\n\t',
         thesis, '\n\n', sep = '')
 }
 
 cat('\n### ', N[2], '\n\n')
 for (i in ordered_certificates) {
+    if (is.null(i$start)) {
+        timeline <- convert_date(i$end)
+    } else {
+        timeline <- convert_date(i$start, i$end)
+    }
     website <- ''
     if (!is.null(i$website))
         website <- paste0('(',i$website,')')
-    cat(i$end, '\n: *',
+    cat(timeline, '\n: *',
         i$title, '*\n\n\t',
         'Organization: ', i$org, '. ', website, '\n\n',
         sep = '')
@@ -41,13 +55,18 @@ for (i in ordered_certificates) {
 
 cat('\n### ', N[3], '\n\n')
 for (i in ordered_courses) {
+    if (is.null(i$start)) {
+        timeline <- convert_date(i$end)
+    } else {
+        timeline <- convert_date(i$start, i$end)
+    }
     website <- ''
     if (!is.null(i$website))
         website <- paste0('(',i$website,')')
 
     if (is.null(i$end))
-        i$end <- "Present"
-    cat(i$end, '\n: *',
+        timeline <- "Present"
+    cat(timeline, '\n: *',
         i$title, '*\n\n\t',
         'Organization: ', i$org, '. ', website, '\n\n',
         sep = '')
