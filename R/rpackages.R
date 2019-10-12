cf609890b5364d97c71cabf9fd31633b97f22067
Luke Johnston <lwjohnst@gmail.com>
2019-10-12 14:58:52 +0200

    Use pkgsearch to get CRAN packages. Resolves #72

--- R/rpackages.R
+++ R/rpackages.R
@@ -1,18 +1,37 @@
 source('R/functions.R')
-output <- yaml::yaml.load_file('_includes/rpackages.yaml')
 
-cat('\n\n### CRAN\n\n')
-for (i in output$rpackages$cran) {
-    cat(i$pkg, '\n: ', paste0('_', i$title, '_: '), i$details, ' *First published*: ',
-        convert_date(i$first_published), '\n\n')
+library(tidyverse)
+library(vitae)
+extract_first_published <- function(pkgs) {
+    pkgs %>%
+        map(~ glue::glue("https://crandb.r-pkg.org/{.x}/all")) %>%
+        map_chr(~ jsonlite::fromJSON(as.character(.x))$versions[[1]]$Date)
 }
 
-cat('\n\n### GitHub\n\n')
-for (i in output$rpackages$github) {
-    cat(i$pkg, '\n: ', i$details, '\n\n')
-}
-
-cat('\n\n### Contributed to\n\n')
-for (i in output$rpackages$contributed) {
-    cat(i$pkg, '\n: ', i$details, '\n\n')
-}
+my_pkgs <- pkgsearch::ps("Luke Johnston")
+my_pkgs %>%
+    mutate(authorship = map_chr(
+        package_data,
+        ~ str_extract(.x$Author, "Luke Johnston.*\\[(aut|ctb)(,|\\])")
+    )) %>%
+    as_tibble() %>%
+    select(package, title, description, date, url, authorship) %>%
+    arrange(desc(date)) %>%
+    mutate(
+        authorship = case_when(
+            str_detect(authorship, "\\[ctb") ~ "contributor",
+            str_detect(authorship, "\\[aut") ~ "creator",
+            TRUE ~ NA_character_
+        )
+    ) %>%
+    filter(!is.na(authorship)) %>%
+    mutate(first_published = package %>%
+               extract_first_published() %>%
+               lubridate::year()) %>%
+    detailed_entries(what = glue::glue("{package}: {title} ({authorship})"),
+                     when = glue::glue("{first_published} (updated: {lubridate::year(date)})"),
+                     with = description,
+                     # TODO: Might need to fix this depending on number of url
+                     where = url) %>%
+    as_tibble() %>%
+    View()
