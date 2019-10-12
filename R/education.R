a1e710fca7856c7a0674a74cf5cbf7e3acdbc641
Luke Johnston <lwjohnst@gmail.com>
2019-10-12 15:40:06 +0200

    use rorcid package instead

--- R/education.R
+++ R/education.R
@@ -1,41 +1,29 @@
+
+library(tidyverse)
+library(glue)
+rorcid::orcid_educations("0000-0003-4169-2616")[[1]]$`affiliation-group`$summaries %>%
+    bind_rows() %>%
+    rename_all( ~ str_remove(., "education-summary\\.")) %>%
+    as_tibble(.name_repair = "universal") %>%
+    select(department.name, role.title, matches("start.date"), matches("end.date"),
+           matches("organization"), url.value) %>%
+    vitae::detailed_entries(
+        what = glue("{role.title} in {department.name}"),
+        when = glue("{start.date.year.value} -- {end.date.year.value}"),
+        with = organization.name,
+        where = str_c(organization.address.city, organization.address.country, sep = ", "),
+        why = url.value
+    ) %>%
+    as.tibble()
+
+
 source('R/functions.R')
 output <- yaml.load_file('_includes/education.yaml')
 n <- names(output)
 N <- gsub('^([a-z])', '\\U\\1', n, perl = TRUE)
 
-ordered_degrees <- order_by_year(output$degrees)
 ordered_certificates <- order_by_year(output$certificates)
 ordered_courses <- order_by_year(output$courses)
-
-cat('\n### ', N[1], '\n\n')
-for (i in ordered_degrees) {
-    if (is.null(i$end)) {
-        timeline <- paste0(convert_date(i$start), ' -- present')
-    } else if (is.null(i$start)) {
-        timeline <- convert_date(i$end)
-    } else {
-        timeline <- convert_date(i$start, i$end)
-    }
-
-    co <- ""
-    if (!is.null(i$co_supervisor))
-        co <- glue("Co-supervisor: {i$co_supervisor}")
-
-    supervisor <- ""
-    if (!is.null(i$supervisor))
-        supervisor <- glue("Supervisor: {i$supervisor}")
-
-    thesis <- ''
-    if (!is.null(i$thesis) & !is.null(i$link)) {
-        thesis <- glue('[{i$thesis}]({i$link}) {supervisor}. {co}')
-    } else if (!is.null(i$thesis) & is.null(i$link)) {
-        thesis <- glue('{i$thesis} {supervisor}. {co}')
-    }
-
-    cat(timeline, '\n: *',
-        i$degree, '*, ', i$university, '\n\n\t',
-        thesis, '\n\n', sep = '')
-}
 
 cat('\n### ', N[2], '\n\n')
 for (i in ordered_certificates) {
