f1999be8eabc56c25074490fe529fd4ebf0fcf21
Luke Johnston <lwjohnst@gmail.com>
2019-11-02 11:35:23 +0100

    don't need these anymore

--- _build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-Rscript -e "rmarkdown::render_site('.', encoding = 'UTF-8')"
-pandoc --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt index.md -o index.pdf
-mv index.pdf cv-full-lukewjohnston.pdf
-pandoc --variable=papersize:"letter" -V geometry:margin=2cm -V fontsize=12pt recent.md -o recent.pdf
-mv recent.pdf cv-recent-lukewjohnston.pdf
-
