17a893947c42196ea8dbb58bbc65c121ed2fcad9
Luke Johnston <lwjohnst@gmail.com>
2019-11-02 18:16:40 +0100

    other wrangling of csv file, to have as a record

--- R/convert-yaml-to-csv.R
+++ R/convert-yaml-to-csv.R
@@ -15,3 +15,22 @@ yaml_to_save %>%
     imap_dfr(~ imap_dfr(.x, as_tibble) %>%
              mutate(section = .y)) %>%
     write_csv(here::here("data/cv-items.csv"))
+
+cv_data <- read_csv("data/cv-items.csv")
+cv_data %>%
+    mutate(role = if_else(!is.na(status), status, role),
+           details = if_else(!is.na(other), other, details),
+           organization = case_when(
+               !is.na(org) ~ org,
+               !is.na(source) ~ source,
+               TRUE ~ NA_character_
+           ),
+           person_name = case_when(
+               !is.na(supervisor) ~ supervisor,
+               !is.na(name) ~ name,
+               TRUE ~ NA_character_
+           )) %>%
+    select(start, end, title, everything()) %>%
+    select(-status, -certificate, -org, -source,
+           -supervisor, -name, -other) %>%
+    write_csv("data/cv-data-2.csv")
