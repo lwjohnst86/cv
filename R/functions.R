254f74f3572fa8d34f2375a1c6aafab57a5e112a
Luke Johnston <lwjohnst@gmail.com>
2019-11-07 18:54:08 +0100

    start of function to create pagedown cv

--- R/functions.R
+++ R/functions.R
@@ -126,3 +126,53 @@ restricted_entry <- function(cv_entry) {
     }
 }
 
+list_education <- function() {
+    orcid <- "0000-0003-4169-2616"
+    orcid_education_data <- rorcid::orcid_educations(orcid)[[1]]
+    education <- orcid_education_data$`affiliation-group`$summaries %>%
+        bind_rows() %>%
+        rename_all(~ str_remove(., "education-summary\\.")) %>%
+        as_tibble(.name_repair = "universal") %>%
+        select(
+            department.name,
+            role.title,
+            matches("start.date"),
+            matches("end.date"),
+            matches("organization"),
+            url.value
+        ) %>%
+        vitae::detailed_entries(
+            what = glue("{role.title} in {department.name}"),
+            when = glue("{start.date.year.value} -- {end.date.year.value}"),
+            with = organization.name,
+            where = str_c(
+                organization.address.city,
+                organization.address.country,
+                sep = ", "
+            ),
+            why = url.value
+        )
+
+    if (interactive()) {
+        education <- education %>%
+            as_tibble()
+    }
+
+    if (knitr::is_html_output()) {
+        education <- education %>%
+            as_tibble() %>%
+            glue_data("
+            ### {with}
+
+            {what}
+
+            {where}
+
+            {when}
+
+            ")
+    }
+
+    education
+}
+
