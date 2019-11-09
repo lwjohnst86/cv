93f9d58a8c8f2f09fb06082d5fddaa5968c13ee9
Luke Johnston <lwjohnst@gmail.com>
2019-11-09 20:55:00 +0100

    package utils and imports

---
+++ R/cv-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @import dplyr
+#' @importFrom here here
+#' @importFrom glue glue glue_data
+#' @importFrom lubridate ymd as_date year
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
