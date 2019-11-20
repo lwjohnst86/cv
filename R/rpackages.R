529bab700c9c8149cf0040fd3867ea014b63780a
Luke Johnston <lwjohnst@gmail.com>
2019-11-20 21:51:44 +0100

    move education and r package functions to own files

---
+++ R/rpackages.R
@@ -0,0 +1,64 @@
+
+extract_first_published <- function(pkgs) {
+    pkgs %>%
+        purrr::map( ~ glue::glue("https://crandb.r-pkg.org/{.x}/all")) %>%
+        purrr::map_chr( ~ jsonlite::fromJSON(as.character(.x))$versions[[1]]$Date)
+}
+
+save_rpackages <- function() {
+    my_pkgs <- pkgsearch::ps("Luke Johnston")
+    rpackages <- my_pkgs %>%
+        dplyr::mutate(authorship = purrr::map_chr(
+            package_data,
+            ~ stringr::str_extract(.x$Author, "Luke Johnston.*\\[(aut|ctb)(,|\\])")
+        )) %>%
+        dplyr::as_tibble() %>%
+        dplyr::select(package, title, description, date, url, authorship) %>%
+        dplyr::arrange(dplyr::desc(date)) %>%
+        dplyr::mutate(
+            authorship = dplyr::case_when(
+                stringr::str_detect(authorship, "\\[ctb") ~ "contributor",
+                stringr::str_detect(authorship, "\\[aut") ~ "creator",
+                TRUE ~ NA_character_
+            )
+        ) %>%
+        dplyr::filter(!is.na(authorship)) %>%
+        dplyr::mutate(first_published = package %>%
+                   extract_first_published() %>%
+                   lubridate::year()) %>%
+        vitae::brief_entries(
+            what = glue::glue("{package}: {title} ({authorship})"),
+            when = first_published,
+            # TODO: Might need to fix this depending on number of url
+            with = "CRAN"
+        )
+    usethis::use_data(rpackages, overwrite = TRUE)
+    return(invisible())
+}
+
+list_rpackages <- function() {
+    data("rpackages", package = "cv")
+
+    if (interactive()) {
+        rpackages <- rpackages %>%
+            as_tibble()
+    }
+
+    if (knitr::is_html_output()) {
+        stop("Fix rpackages gluing.")
+        rpackages <- rpackages %>%
+            as_tibble() %>%
+            glue_data("
+            ### {with}
+
+            {what}
+
+            {where}
+
+            {when}
+
+            ")
+    }
+
+    rpackages
+}
