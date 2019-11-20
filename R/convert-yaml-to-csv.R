6d22a6624d51cffff077b5a06f0a063f773d272e
Luke Johnston <lwjohnst@gmail.com>
2019-11-20 21:51:06 +0100

    convert to function

--- R/convert-yaml-to-csv.R
+++ R/convert-yaml-to-csv.R
@@ -1,36 +1,38 @@
-library(tidyverse, quietly = TRUE)
 
-yaml_files <- fs::dir_ls(here::here("_includes/"), glob = "*.yaml")
+# Used this to convert my old yaml data files into csv.
+convert_yaml_to_csv <- function() {
+    yaml_files <- fs::dir_ls(here("_includes/"), glob = "*.yaml")
 
-yaml_to_save <- yaml_files %>%
-    str_remove(".*(publications|misc).*") %>%
-    na_if("") %>%
-    na.omit()
+    yaml_to_save <- yaml_files %>%
+        str_remove(".*(publications|misc).*") %>%
+        na_if("") %>%
+        na.omit()
 
-basename(yaml_to_save)
+    basename(yaml_to_save)
 
-yaml_to_save %>%
-    map(yaml::read_yaml) %>%
-    flatten() %>%
-    imap_dfr(~ imap_dfr(.x, as_tibble) %>%
-             mutate(section = .y)) %>%
-    write_csv(here::here("data/cv-items.csv"))
+    yaml_to_save %>%
+        map(yaml::read_yaml) %>%
+        flatten() %>%
+        imap_dfr( ~ imap_dfr(.x, as_tibble) %>%
+                      mutate(section = .y)) %>%
+        write_csv(here("data/cv-items.csv"))
 
-cv_data <- read_csv("data/cv-items.csv")
-cv_data %>%
-    mutate(role = if_else(!is.na(status), status, role),
-           details = if_else(!is.na(other), other, details),
-           organization = case_when(
-               !is.na(org) ~ org,
-               !is.na(source) ~ source,
-               TRUE ~ NA_character_
-           ),
-           person_name = case_when(
-               !is.na(supervisor) ~ supervisor,
-               !is.na(name) ~ name,
-               TRUE ~ NA_character_
-           )) %>%
-    select(start, end, title, everything()) %>%
-    select(-status, -certificate, -org, -source,
-           -supervisor, -name, -other) %>%
-    write_csv("data/cv-data-2.csv")
+    cv_data <- read_csv("data/cv-items.csv")
+    cv_data %>%
+        mutate(
+            role = if_else(!is.na(status), status, role),
+            details = if_else(!is.na(other), other, details),
+            organization = case_when(!is.na(org) ~ org,
+                                     !is.na(source) ~ source,
+                                     TRUE ~ NA_character_),
+            person_name = case_when(
+                !is.na(supervisor) ~ supervisor,
+                !is.na(name) ~ name,
+                TRUE ~ NA_character_
+            )
+        ) %>%
+        select(start, end, title, everything()) %>%
+        select(-status, -certificate, -org, -source,
+               -supervisor, -name, -other) %>%
+        write_csv(here("data/cv-data-2.csv"))
+}
--- R/send-to-figshare.R
+++ R/send-to-figshare.R
@@ -1,18 +1,35 @@
-library(rfigshare)
 
-id <- fs_new_article("Triglyceride Fatty Acid (TGFA) Composition Longitudinally Associates with Changes in Insulin Sensitivity (IS) and Beta-Cell Function Over 6-yrs in the Prospective Metabolism and Islet Cell Evaluation (PROMISE) Cohort",
-          "Poster presented at the American Diabetes Association 76th Scientific Sessions from June 10-14, 2016 in New Orleans, USA.
+send_to_figshare <- function() {
+    # TODO: Fix this.
+    rfigshare::fs_new_article(
+            "Triglyceride Fatty Acid (TGFA) Composition Longitudinally Associates with Changes in Insulin Sensitivity (IS) and Beta-Cell Function Over 6-yrs in the Prospective Metabolism and Islet Cell Evaluation (PROMISE) Cohort",
+            "Poster presented at the American Diabetes Association 76th Scientific Sessions from June 10-14, 2016 in New Orleans, USA.
 
           Full citation:
           ",
-          type = "poster",
-          links = "https://doi.org/10.2337/db16-1-381",
-          authors = c("Luke W. Johnston", "Stewart Harris", "Ravi Retnakaran",
-                           "Bernard Zinman", "Zhen Liu", "Richard P. Bazinet",
-                           "Anthony J. Hanley"),
-          tags = c("insulin sensitivity", "beta-cell function", "fatty acids",
-                        "triacylglycerols", "cohort", "longitudinal"),
-          categories = c("Diseases", "Pathogenesis", "Epidemiology"),
-          files = "_research/poster-2016-06-ADA.pdf",
-          visibility = "public")
+            type = "poster",
+            links = "https://doi.org/10.2337/db16-1-381",
+            authors = c(
+                "Luke W. Johnston",
+                "Stewart Harris",
+                "Ravi Retnakaran",
+                "Bernard Zinman",
+                "Zhen Liu",
+                "Richard P. Bazinet",
+                "Anthony J. Hanley"
+            ),
+            tags = c(
+                "insulin sensitivity",
+                "beta-cell function",
+                "fatty acids",
+                "triacylglycerols",
+                "cohort",
+                "longitudinal"
+            ),
+            categories = c("Diseases", "Pathogenesis", "Epidemiology"),
+            files = "_research/poster-2016-06-ADA.pdf",
+            visibility = "public"
+        )
+}
+
 
