a76902676d3a88f12167770fe87b2d38a090c079
Luke Johnston <lwjohnst@gmail.com>
2019-12-15 19:16:06 +0100

    rearrange generate cv so pdf and html output are diff functions

--- R/generate-cv.R
+++ R/generate-cv.R
@@ -1,18 +1,26 @@
 generate_cv <- function() {
-    # PDF
+    generate_cv_pdf()
+    generate_cv_html()
+}
+
+generate_cv_pdf <- function() {
     rmarkdown::render(
         input = here("vignettes", "cv.Rmd"),
         output_format = "vitae::awesomecv",
-        output_dir = here("docs"),
-        quiet = TRUE
-    )
-
-    # HTML
-    rmarkdown::render(
-        input = here("vignettes", "cv.Rmd"),
-        output_format = "pagedown::html_resume",
+        encoding = "UTF-8",
+        knit_root_dir = here(),
         output_dir = here("docs"),
         quiet = TRUE
     )
 }
 
+generate_cv_html <- function() {
+    rmarkdown::render(
+        input = here("vignettes", "cv.Rmd"),
+        output_format = "pagedown::html_resume",
+        encoding = "UTF-8",
+        knit_root_dir = here(),
+        output_dir = here("docs"),
+        quiet = TRUE
+    )
+}
