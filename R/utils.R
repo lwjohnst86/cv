bb23eea8f5df576e1c37246e868962d5bc483dea
Luke Johnston <lwjohnst@gmail.com>
2020-01-06 20:14:36 +0100

    start fixing up output for listing pubs, add timestamp fun

--- R/utils.R
+++ R/utils.R
@@ -10,7 +10,6 @@ convert_to_cvhonors <- function(.tbl) {
 
 output <- function(.object, compact = FALSE, from_bib = FALSE) {
     if (knitr::is_html_output()) {
-        cat("HTML\n\n")
         .object <- .object %>%
             tibble::as_tibble() %>%
             dplyr::mutate_all( ~ dplyr::if_else(is.na(.), "N/A", as.character(.)))
@@ -23,32 +22,49 @@ output <- function(.object, compact = FALSE, from_bib = FALSE) {
         }
 
         if (from_bib) {
-            .object
+            .output_html_resume_bib(.object)
         } else {
-            .object %>%
-                glue_data("
-
-                ### {with}
-
-                {what}
-
-                {where}
-
-                {when}
-
-                ")
+            .output_html_resume_item(.object)
         }
 
     } else if (knitr::is_latex_output()) {
-        cat("PDF\n\n")
         .object
     } else if (interactive()) {
-        cat("INTERACTIVE\n\n")
         .object %>%
-            tibble::as_tibble()
+            .output_html_resume_bib()
+            # tibble::as_tibble()
     }
 }
 
+.output_html_resume_bib <- function(.tbl) {
+    .tbl %>%
+        tibble::remove_rownames() %>%
+        tibble::column_to_rownames("key") %>%
+        RefManageR::as.BibEntry()
+}
+
+.output_html_resume_item <- function(.tbl) {
+    .tbl %>%
+        glue_data("
+
+        ### {with}
+
+        {what}
+
+        {where}
+
+        {when}
+
+        ")
+}
+
+#' Tidy up date variables.
+#'
+#' @param .tbl Dataset to tidy with `start` and `end` date variables.
+#'
+#' @return A [tibble::tibble()] dataset with tidied date variables.
+#' @export
+#'
 tidy_dates <- function(.tbl) {
     .tbl %>%
         dplyr::arrange(dplyr::desc(is.na(end)), dplyr::desc(start)) %>%
@@ -58,4 +74,13 @@ tidy_dates <- function(.tbl) {
             end = dplyr::if_else(is.na(end), "present", as.character(end)),
             date_range = dplyr::if_else(start == end, glue("{start}"), glue("{start} -- {end}"))
         )
+}
+
+#' Timestamp for today.
+#'
+#' @return Outputs string with date as "March 1, 1999"
+#' @export
+#'
+today_timestamp <- function() {
+    lubridate::stamp('March 1, 1999', quiet = TRUE)(lubridate::today())
 }
