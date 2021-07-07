34712f7a5f9d21fc0661468d58fdcdc1cf398df4
Luke W. Johnston <lwjohnst@gmail.com>
2021-07-07 14:53:28 +0200

    merge presentations into one and add affiliations

---
+++ R/affiliations.R
@@ -0,0 +1,14 @@
+#' @describeIn list_sections List all affiliations.
+#' @export
+list_affiliations <- function(.tbl) {
+    .tbl %>%
+        dplyr::filter(section == "affiliations") %>%
+        tidy_dates() %>%
+        vitae::detailed_entries(
+            what = role,
+            when = date_range,
+            with = organization,
+            where = location
+        ) %>%
+        output()
+}
--- R/publications.R
+++ R/publications.R
@@ -3,14 +3,18 @@
 list_publications <- function(.tbl, .type) {
     pub_format <- tibble::tribble(
         ~category, ~glue_exp,
-        "article", "1. {author}. '{title}'. *{journal}* ({year}). {doi}",
-        "inproceedings", "1. {author}. '{title}'. *{booktitle}* in {address} ({year}). {doi}",
+        "article", "1. {presentation_type} {author}. '{title}'. *{journal}* ({year}). {doi}",
+        "inproceedings", "1. {presentation_type} {author}. '{title}'. *{booktitle}* in {address} ({year}). {doi}",
         "misc", "1. {author}. '{title}'. ({year}). {url} {doi}"
     )
 
     format_prep <- .tbl %>%
         dplyr::arrange(dplyr::desc(year)) %>%
         dplyr::mutate(
+            presentation_type = stringr::str_remove(groups, " presentations"),
+            presentation_type = if_else(stringr::str_detect(groups, "presentations"),
+                                        as.character(glue::glue("({presentation_type})")),
+                                        ""),
             category = tolower(category),
             doi = dplyr::if_else(
                 is.na(doi),
@@ -26,7 +30,7 @@ list_publications <- function(.tbl, .type) {
                 stringr::str_replace("(L(uke|\\.)?( ?W\\.?)? Johnston)", "**\\1**"),
         ) %>%
         dplyr::filter(category %in% c("article", "inproceedings", "misc")) %>%
-        dplyr::filter(groups == .type) %>%
+        dplyr::filter(stringr::str_detect(groups, .type)) %>%
         tidyr::nest(data = !category) %>%
         dplyr::left_join(pub_format, by = "category")
 
