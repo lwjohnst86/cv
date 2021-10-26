eb48272659a0b18b13e5cecf0a4bb7d0595f7542
Luke W. Johnston <lwjohnst@gmail.com>
2021-10-26 16:48:59 +0200

    add committee section with DDA WS. Closes #114

---
+++ R/committees.R
@@ -0,0 +1,14 @@
+#' @describeIn list_sections List all committees.
+#' @export
+list_committees <- function(.tbl) {
+    .tbl %>%
+        dplyr::filter(section == "committee") %>%
+        tidy_dates() %>%
+        vitae::detailed_entries(
+            what = role,
+            when = date_range,
+            with = organization,
+            where = location
+        ) %>%
+        output()
+}
