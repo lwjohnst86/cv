caf56c31c9c1dd2bc59d14ace552c1f268b00652
Luke W. Johnston <lwjohnst@gmail.com>
2021-10-26 17:11:51 +0200

    add book pubs to function

--- R/publications.R
+++ R/publications.R
@@ -5,12 +5,14 @@ list_publications <- function(.tbl, .type) {
         ~category, ~glue_exp,
         "article", "1. {presentation_type} {author}. '{title}'. *{journal}* ({year}). {doi}",
         "inproceedings", "1. {presentation_type} {author}. '{title}'. *{booktitle}* in {address} ({year}). {doi}",
+        "book", "1. {presentation_type} {author}. '{title}'. *{publisher}* ({year}). {url} {isbn}",
         "misc", "1. {author}. '{title}'. ({year}). {url} {doi}"
     )
 
     format_prep <- .tbl %>%
         dplyr::arrange(dplyr::desc(year)) %>%
         dplyr::mutate(
+            title = stringr::str_remove_all(title, "\\{|\\}"),
             presentation_type = stringr::str_remove(groups, " presentations"),
             presentation_type = if_else(stringr::str_detect(groups, "presentations"),
                                         as.character(glue::glue("({presentation_type})")),
@@ -26,10 +28,15 @@ list_publications <- function(.tbl, .type) {
                 "",
                 paste0("URL: <", url, ">.")
             ),
+            isbn = dplyr::if_else(
+                is.na(isbn),
+                "",
+                paste0("ISBN: [", isbn, "](https://isbnsearch.org/isbn/", isbn, ").")
+            ),
             author = purrr::map_chr(author, ~ stringr::str_c(.x$full_name, collapse = ", ")) %>%
                 stringr::str_replace("(L(uke|\\.)?( ?W\\.?)? Johnston)", "**\\1**"),
         ) %>%
-        dplyr::filter(category %in% c("article", "inproceedings", "misc")) %>%
+        dplyr::filter(category %in% c("article", "inproceedings", "misc", "book")) %>%
         dplyr::filter(stringr::str_detect(groups, .type)) %>%
         tidyr::nest(data = !category) %>%
         dplyr::left_join(pub_format, by = "category")
