a6750cf39768a72720550f717444668393dc03e1
Luke W. Johnston <lwjohnst@gmail.com>
2021-10-31 21:38:05 +0100

    add award amount

--- R/awards.R
+++ R/awards.R
@@ -4,9 +4,11 @@ list_awards <- function(.tbl) {
     .tbl %>%
         dplyr::filter(section == "awards") %>%
         dplyr::mutate(start = dplyr::if_else(is.na(start), end, start),
-                      year = lubridate::year(start)) %>%
+                      year = lubridate::year(start),
+                      amount = dplyr::if_else(is.na(value), "",
+                                              paste0("(", value, ")"))) %>%
         vitae::brief_entries(
-            what = glue("{title} from {organization}"),
+            what = glue::glue("{title} from {organization} {amount}"),
             when = year,
             with = location
         ) %>%
