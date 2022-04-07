696ff9f9725fcfba6c6049cf0c34ea901c755889
Luke W. Johnston <lwjohnst@gmail.com>
2022-04-07 19:04:14 +0200

    add NNF DIF funding. Closes #121

---
+++ R/funding.R
@@ -0,0 +1,16 @@
+#' @describeIn list_sections List all funding and grants given.
+#' @export
+list_funding <- function(.tbl) {
+    .tbl %>%
+        dplyr::filter(section == "funding") %>%
+        dplyr::mutate(start = dplyr::if_else(is.na(start), end, start),
+                      year = lubridate::year(start),
+                      amount = dplyr::if_else(is.na(value), "",
+                                              paste0("(", value, ")"))) %>%
+        vitae::brief_entries(
+            what = glue::glue("{title} from {organization} {amount}"),
+            when = year,
+            with = location
+        ) %>%
+        output()
+}
