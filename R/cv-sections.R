fd28f19074e71c37e092ea7cdd108da77e485ce4
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-30 15:37:07 +0200

    don't need this function anymore

--- R/cv-sections.R
+++ R/cv-sections.R
@@ -48,21 +48,6 @@ list_committees <- function(data, caption = NULL) {
     output(caption = caption)
 }
 
-#' @describeIn list_sections List all employment activities.
-#' @export
-list_employment <- function(data, caption = NULL) {
-  data |>
-    dplyr::filter(section == "employment") |>
-    tidy_dates() |>
-    vitae::detailed_entries(
-      what = role,
-      when = date_range,
-      with = organization,
-      where = location
-    ) |>
-    output(caption = caption)
-}
-
 #' @describeIn list_sections List all funding and grants given.
 #' @export
 list_funding <- function(data) {
