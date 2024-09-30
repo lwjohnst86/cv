39cc51a60d9f08c0048731c33a7053cf9a26b4fc
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-30 16:12:44 +0200

    don't need anymore

--- R/cv-sections.R
+++ R/cv-sections.R
@@ -13,26 +13,6 @@ list_affiliations <- function(data) {
     output()
 }
 
-#' @describeIn list_sections List all awards given.
-#' @export
-list_awards <- function(data, caption = NULL) {
-  data |>
-    dplyr::filter(section == "awards") |>
-    dplyr::mutate(
-      start = dplyr::if_else(is.na(start), end, start),
-      year = lubridate::year(start),
-      amount = dplyr::if_else(is.na(value), "",
-        paste0("(", value, ")")
-      )
-    ) |>
-    vitae::brief_entries(
-      what = glue::glue("{title} from {organization} {amount}"),
-      when = year,
-      with = location
-    ) |>
-    output(caption = caption)
-}
-
 #' @describeIn list_sections List all funding and grants given.
 #' @export
 list_funding <- function(data) {
