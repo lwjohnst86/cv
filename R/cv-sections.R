e32cd48e47ecea4c6b8ebcfbecbe7eb2d65aa85f
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-30 15:47:03 +0200

    move committee entries into YAML

--- R/cv-sections.R
+++ R/cv-sections.R
@@ -33,21 +33,6 @@ list_awards <- function(data, caption = NULL) {
     output(caption = caption)
 }
 
-#' @describeIn list_sections List all committees.
-#' @export
-list_committees <- function(data, caption = NULL) {
-  data |>
-    dplyr::filter(section == "committee") |>
-    tidy_dates() |>
-    vitae::detailed_entries(
-      what = title,
-      when = date_range,
-      with = organization,
-      where = location
-    ) |>
-    output(caption = caption)
-}
-
 #' @describeIn list_sections List all funding and grants given.
 #' @export
 list_funding <- function(data) {
