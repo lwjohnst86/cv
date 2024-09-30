99fa1704a982576924a0ccfed120df4910ce49fa
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-30 16:38:02 +0200

    move funding into YAML

--- R/cv-sections.R
+++ R/cv-sections.R
@@ -13,26 +13,6 @@ list_affiliations <- function(data) {
     output()
 }
 
-#' @describeIn list_sections List all funding and grants given.
-#' @export
-list_funding <- function(data) {
-  data |>
-    dplyr::filter(section == "funding") |>
-    dplyr::mutate(
-      start = dplyr::if_else(is.na(start), end, start),
-      year = lubridate::year(start),
-      amount = dplyr::if_else(is.na(value), "",
-        paste0("(", value, ")")
-      )
-    ) |>
-    vitae::brief_entries(
-      what = glue::glue("{title} from {organization} {amount}"),
-      when = year,
-      with = location
-    ) |>
-    output()
-}
-
 #' @describeIn list_sections List all outreach activities.
 #' @export
 list_outreach <- function(data, caption = NULL) {
