8581008023330bf6088212fcffc5943dea11cf1f
Luke W. Johnston <lwjohnst@gmail.com>
2024-10-01 12:24:25 +0200

    moved affiliations into YAML

--- R/cv-sections.R
+++ R/cv-sections.R
@@ -1,18 +1,3 @@
-#' @describeIn list_sections List all affiliations.
-#' @export
-list_affiliations <- function(data) {
-  data |>
-    dplyr::filter(section == "affiliations") |>
-    tidy_dates() |>
-    vitae::detailed_entries(
-      what = role,
-      when = date_range,
-      with = organization,
-      where = location
-    ) |>
-    output()
-}
-
 #' @describeIn list_sections List all publications.
 #' @export
 list_publications <- function(data, .type) {
